ea285b7f77924fb09600ba9dbf905f10f2105365
Andreas Otten <andreas.otten@atlascopco.com>
2021-01-28 23:18:47 +0100

    Carousel creation + HomePage navigation

---
+++ DevShop2.0/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using DevShop2.Models;
+using DevShop2.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevShop2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public HomeController(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                CoursesOfTheWeek = _courseRepository.CoursesOfTheWeek
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ DevShop2.0/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using DevShop2.Models;
+using System.Collections.Generic;
+
+namespace DevShop2.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Course> CoursesOfTheWeek { get; set; }
+    }
+}
